bbcc258be979ce642cc95d5577a28e6ffa68d9b3
Miguel Angel Franco Leon <vandammejeanclaude337@gmail.com>
2026-01-23 05:22:32 -0300

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,5 @@
+from .constants import CHI, DELTA_F, RU, FRACTAL_SCALE
+from .engine import franco_reduction_law, apply_mfsu_transform
+
+__version__ = "2.0.0-Diamond"
+__all__ = ['CHI', 'DELTA_F', 'RU', 'apply_mfsu_transform', 'franco_reduction_law']
